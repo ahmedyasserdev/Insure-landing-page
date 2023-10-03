ec4c4dd22526852ea3da25cb3f4f3df83b11f65f
ahmedyasserdev <ahmedyasser3033@gmail.com>
2023-10-03 20:24:11 +0300

    added animation to the icons

--- js/main.js
+++ js/main.js
@@ -13,3 +13,36 @@ toggler.addEventListener("click" , () => {
     }
     ul.classList.toggle("show")
 } )
+
+// Get the icons element
+const icons = document.querySelectorAll('.icons img');
+const footer = document.querySelector('.footer');
+const footerOffset = footer.offsetTop -200 ;
+console.log(footerOffset)
+
+icons.forEach((icon) => {
+    icon.style.transition = 'transform 2.5s ease';
+});
+
+function handleScroll() {
+    const scrollPosition = window.scrollY;
+
+  // Calculate the rotation angle based on the scroll position
+  const rotationAngle = scrollPosition === footerOffset ? 0 : -360;
+
+  // Apply the rotation to the icons
+  icons.forEach((icon) => {
+    icon.style.transform = `rotate(${rotationAngle}deg)`;
+  });
+
+  // Reset the rotation when scroll position is at the top
+  if (scrollPosition >= footerOffset || scrollPosition === footerOffset  ) {
+    icons.forEach((icon) => {
+      icon.style.transform = 'rotate(0deg)';
+    });
+  }
+}
+
+
+// Add scroll event listener
+window.addEventListener('scroll', handleScroll);
